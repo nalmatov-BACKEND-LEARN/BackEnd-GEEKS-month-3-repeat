1ee10d7509228e65630d6b1d97335b0b83c1ebd7
nalmatov <nalmatov6@gmail.com>
2025-02-15 16:36:09 +0600

    first homework

--- main.py
+++ main.py
@@ -11,8 +11,23 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-  await message.answer('hello world')
+  await message.answer('Hello world!')
 
+@dp.message_handler(commands=['image'])
+async def image_handler(message: types.Message):
+  photo = open('media/image.jpeg', 'rb')
+  await bot.send_photo(chat_id=message.from_id.id,
+                       photo=photo)
+
+@dp.message_handler()
+async def echo_handler(message: types.Message):
+  try:
+    num = float(message.text)
+    await message.answer(int(num**2))
+    return
+  except: pass
+
+  await message.answer(message.text)
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO)
